9a93535ff35371817f56c7a6007a14cec6c734d7
Anand Singh <pb11aps97@gmail.com>
2024-01-04 19:58:36 -0500

    link to github projects

--- portfolio/src/Components/Projects.jsx
+++ portfolio/src/Components/Projects.jsx
@@ -6,11 +6,11 @@ import reactParallax from "../assets/portfolio/reactParallax.jpg";
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
 import { description } from "../assets/description";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   const [projectDescription, setProjectDescription] = useState(null);
-  const [isShowDetailTrue, setShowDetails] = useState(false);
-  const [details, setDetails] = useState('');
+  const [details, setDetails] = useState("");
 
   const projects = [
     { id: 1, src: installNode },
@@ -23,53 +23,54 @@ const Projects = () => {
 
   const showDescription = (id) => {
     setProjectDescription(id);
+    setDetails(description[0]);
   };
 
   const hideDescription = () => {
     setProjectDescription(null);
   };
 
-  const showDetails = (id) => {
-    setDetails(description[id - 1])
-    setShowDetails(true);
-  };
-
   return (
-    <div name="Projects" className="w-full relative text-gray-700 h-fit mb-[9rem]">
-      {isShowDetailTrue && (
-        <div className="absolute border-2 z-10 h-fit w-[90%] bg-blue-100 text-lg left-[5%]">
-          <h1 className="flex justify-center">{details.title}</h1>
-          <div className="">
-            <p className="border-2 text-center w-fit">{details.description}</p>
-          </div>
-        </div>
-      )}
-
+    <div
+      name="Projects"
+      className="w-full relative text-gray-700 h-fit mb-[9rem]"
+    >
       <div className="pb-8 flex flex-col justify-center items-center">
         <p className="text-4xl font-bold inline border-b-2 pb-1 border-gray-500">
           Projects
         </p>
       </div>
 
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-[5rem]">
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-5 sm:px-[5rem]">
         {projects.map((element) => (
-          <div key={element.id} className="shadow-md shadow-blue-100 rounded-lg overflow-hidden">
-            <div onClick={() => showDetails(element.id)} className="relative" onMouseEnter={() => showDescription(element.id)} onMouseLeave={hideDescription}>
+          <div
+            key={element.id}
+            className="shadow-md shadow-blue-100 rounded-lg overflow-hidden"
+          >
+            <div
+              className="relative  overflow-hidden"
+              onMouseEnter={() => showDescription(element.id)}
+              onMouseLeave={hideDescription}
+            >
               <img className="rounded-md" src={element.src} alt="" />
               {projectDescription === element.id && (
-                <div className="backdrop-blur-lg top-0 w-full h-full absolute bg-white bg-opacity-[0.1]">
-                  <p>krekfrkf</p>
+                <div className="backdrop-blur-lg top-0 p-2 w-full h-full absolute bg-white bg-opacity-[0.1]">
+                  <div className="text-center">
+                    <h1 className=" text-lg font-bold">{details.title}</h1>
+                  </div>
+                  <div className="text-center text-[1.02rem]">
+                    <p>{details.description}</p>
+                  </div>
                 </div>
               )}
             </div>
 
             <div className="flex justify-center items-center">
-              <button className="font-semibold bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer rounded-lg w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                Demo
-              </button>
-              <button className="font-semibold bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer rounded-lg w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+              <Link
+                to="https://github.com/Anand-singh97/BlogApp_MERN"
+                className="font-semibold text-center bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer rounded-lg w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                 Code
-              </button>
+              </Link>
             </div>
           </div>
         ))}
--- portfolio/src/assets/description.js
+++ portfolio/src/assets/description.js
@@ -4,7 +4,7 @@ export const description =
 [
     {
         title:'Blog App', 
-        description:'A full-stack Blog Application developed using the MERN'+ 
-        '(MongoDB, Express.js, React.js, Node.js) stack.'
+        code: 'https://github.com/Anand-singh97/BlogApp_MERN',
+        description:'This project is a full-stack Blog Application developed using the MERN (MongoDB, Express.js, React.js, Node.js) stack. It includes user authentication functionalities with Passport.js for Google sign-in and secure JWT token storage in cookies. Middleware has been implemented for user verification, checking token expiry, and encryption.'
     }
 ];
